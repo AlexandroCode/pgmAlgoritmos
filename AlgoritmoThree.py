f3bac9b3e5e983dd42d623564a6ba361090bb59d
AlexandroCode <armenttamario@gmail.com>
2016-11-01 22:06:32 -0700

    Tercer Algoritmo añadido

---
+++ AlgoritmoThree.py
@@ -0,0 +1,41 @@
+from MainCode import *
+
+p1 = instrucciones([-3, -6, -2, 8], [], [0, 0, 0, 1], 0, 0, 1) #constructor
+
+while (len(p1.listaEntrada) != 0 or p1.monito > 0):
+
+    if  p1.paso == 1:
+        p1.entrada()
+
+    if  p1.paso == 2:
+        p1.copiarA(0)
+
+    if  p1.paso == 3:
+        p1.salida()
+
+    if  p1.paso == 4:
+        p1.copiarDe(0)
+
+    if  p1.paso == 5:
+        p1.irNeg(9)
+
+    if  p1.paso == 6:
+        p1.menosmenos()
+
+    if  p1.paso == 7:
+        p1.irCero(11)
+
+    if  p1.paso == 8:
+        p1.irA(2)
+
+    if  p1.paso == 9:
+        p1.masmas()
+
+    if p1.paso == 10:
+        p1.irNeg(2)
+
+    if p1.paso == 11:
+        p1.salida()
+
+    if p1.paso == 12:
+        p1.irA(1)
